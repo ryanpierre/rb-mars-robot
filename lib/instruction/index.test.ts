1f5f23d33c8ebfbad5506c4e6780a46fad2da58a
Ryan Pierre <info@ryanpierre.com>
2023-05-22 00:42:13 +0100

    Add validation and change instantiation to a factory pattern

--- lib/instruction/index.test.ts
+++ lib/instruction/index.test.ts
@@ -1,9 +1,12 @@
-import { Instruction } from "."
+import { Instruction } from ".";
 
 describe("Instruction", () => {
-    it("initialises with valid input", () => {
-        const instruction = new Instruction("L")
+  it("initialises with valid input", () => {
+    const instruction = Instruction.create("L");
 
-        expect(instruction.command).toEqual("L")
-    })
-})
+    expect(instruction.command).toEqual("L");
+  });
+  it("throws an error if the supplied input is invalid", () => {
+    expect(() => Instruction.create("Q")).toThrow("Invalid command");
+  });
+});
--- lib/instruction/index.ts
+++ lib/instruction/index.ts
@@ -1,11 +1,21 @@
+const VALID_INSTRUCTIONS = ["L", "R", "F"];
+
 export class Instruction {
   private _command: string;
 
-  constructor(input: string) {
+  private constructor(input: string) {
     this._command = input;
   }
 
   public get command() {
     return this._command;
+  }
+
+  public static create(input: string) {
+    if (!VALID_INSTRUCTIONS.includes(input)) {
+      throw new Error("Invalid command");
+    }
+
+    return new Instruction(input);
   }
 }
