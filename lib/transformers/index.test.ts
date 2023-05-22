2d91a25aadb158ae1a206e9e7a0e27f22e4da656
Ryan Pierre <info@ryanpierre.com>
2023-05-22 01:33:52 +0100

    add counter clockwise transformer

--- lib/transformers/index.test.ts
+++ lib/transformers/index.test.ts
@@ -1,4 +1,4 @@
-import { rotateClockwise } from ".";
+import { rotateClockwise, rotateCounterClockwise } from ".";
 
 describe("Transformers", () => {
   describe("rotateClockwise", () => {
@@ -9,6 +9,16 @@ describe("Transformers", () => {
     it("always ensures direction is within 0 and 3", () => {
       const position = { x: 0, y: 0, d: 3 };
       expect(rotateClockwise(position)).toEqual({ x: 0, y: 0, d: 0 });
+    });
+  });
+  describe("rotateCounterClockwise", () => {
+    it("rotates a position counter clockwise", () => {
+      const position = { x: 0, y: 0, d: 3 };
+      expect(rotateCounterClockwise(position)).toEqual({ x: 0, y: 0, d: 2 });
+    });
+    it("always ensures direction is within 0 and 3", () => {
+      const position = { x: 0, y: 0, d: 0 };
+      expect(rotateCounterClockwise(position)).toEqual({ x: 0, y: 0, d: 3 });
     });
   });
 });
--- lib/transformers/index.ts
+++ lib/transformers/index.ts
@@ -3,3 +3,7 @@ import { Position } from "../instruction";
 export const rotateClockwise = (p: Position) => {
   return { ...p, d: (p.d + 1) % 4 };
 };
+
+export const rotateCounterClockwise = (p: Position) => {
+  return { ...p, d: p.d - 1 >= 0 ? p.d - 1 : p.d + 3 };
+};
