f8eb19fd407fbeed5fb488d315342bce199677fd
Ryan Pierre <info@ryanpierre.com>
2023-05-22 13:06:08 +0100

    add the ability to set a robots status as lost

--- lib/robot/index.test.ts
+++ lib/robot/index.test.ts
@@ -49,4 +49,22 @@ describe("Robot", () => {
     expect(testTransformer).toHaveBeenCalledWith({ x: 0, y: 0, d: 0 });
     expect(robot.position).toEqual({ x: 0, y: 0, d: 0 });
   });
+
+  it("sets the lost state to true when a robot moves off the map", () => {
+    const testGrid = new MockGrid(10, 10);
+    const testTransformer = jest.fn(() => ({ x: 11, y: 10, d: 0 }));
+    const testInstruction = jest
+      .fn()
+      .mockReturnValue({ command: testTransformer });
+    const robot = new Robot({ x: 0, y: 0, d: 0 }, testGrid);
+
+    testGrid.isWithinBoundaries = jest.fn().mockReturnValue(false);
+    testGrid.validate = jest.fn().mockReturnValue(true);
+
+    robot.action(testInstruction());
+
+    expect(testTransformer).toHaveBeenCalledWith({ x: 0, y: 0, d: 0 });
+    expect(robot.position).toEqual({ x: 11, y: 10, d: 0 });
+    expect(robot.isLost).toEqual(true);
+  });
 });
--- lib/robot/index.ts
+++ lib/robot/index.ts
@@ -4,10 +4,12 @@ import { Instruction } from "../instruction";
 export class Robot {
   private _position: Position;
   private _grid: Grid;
+  private _isLost: boolean;
 
-  constructor(position: Position, grid: Grid) {
+  constructor(position: Position, grid: Grid, isLost: boolean = false) {
     this._position = position;
     this._grid = grid;
+    this._isLost = isLost;
   }
 
   public get position() {
@@ -18,11 +20,20 @@ export class Robot {
     return this._grid;
   }
 
+  public get isLost() {
+    return this._isLost;
+  }
+
   public action(i: Instruction) {
     const proposedPosition = i.command(this.position);
+    const { x, y } = proposedPosition;
 
-    this._position = this.grid.validate(proposedPosition.x, proposedPosition.y)
+    this._position = this.grid.validate(x, y)
       ? proposedPosition
       : this._position;
+
+    if (!this.grid.isWithinBoundaries(x, y)) {
+      this._isLost = true;
+    }
   }
 }
