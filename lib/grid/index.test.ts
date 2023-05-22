ed45aa4d0c226bc5e4bf63b81e0dd1c193acd8b8
Ryan Pierre <info@ryanpierre.com>
2023-05-22 01:58:48 +0100

    Add the ability for a grid to store scents

--- lib/grid/index.test.ts
+++ lib/grid/index.test.ts
@@ -6,4 +6,9 @@ describe("Grid", () => {
 
     expect(grid.boundaries).toEqual({ xMin: 0, xMax: 3, yMin: 0, yMax: 2 });
   });
+  it("should initialise with the provided array of known Scents", () => {
+    const grid = new Grid(3, 2, [{ x: 3, y: 0 }]);
+
+    expect(grid.scents).toEqual([{ x: 3, y: 0 }]);
+  });
 });
--- lib/grid/index.ts
+++ lib/grid/index.ts
@@ -1,13 +1,21 @@
+type Scent = { x: number; y: number };
+
 export class Grid {
   private _xMax: number;
   private _yMax: number;
+  private _scents: Scent[];
 
-  constructor(xMax: number, yMax: number) {
+  constructor(xMax: number, yMax: number, scents: Scent[] = []) {
     this._xMax = xMax;
     this._yMax = yMax;
+    this._scents = scents;
   }
 
   public get boundaries() {
     return { xMin: 0, xMax: this._xMax, yMin: 0, yMax: this._yMax };
+  }
+
+  public get scents() {
+    return this._scents;
   }
 }
