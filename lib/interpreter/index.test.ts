24d6b1e3b5aa9a6e74d220ad056912739dbbcfe1
Ryan Pierre <info@ryanpierre.com>
2023-05-22 16:03:56 +0100

    add ability to parse a position

--- lib/interpreter/index.test.ts
+++ lib/interpreter/index.test.ts
@@ -6,9 +6,13 @@ const SAMPLE_DATA = `5 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFF
 jest.mock("../grid");
 
 describe("Interpreter", () => {
-  it("takes the first line of input and returns a grid", () => {
+  it("takes a boundary string and returns a Grid", () => {
     const grid = Interpreter.parseGrid("5 3");
     expect(Grid).toHaveBeenCalledWith(5, 3);
     expect(grid).toBeInstanceOf(Grid);
+  });
+  it("takes a position string and returns a Position", () => {
+    const position = Interpreter.parsePosition("1 1 E");
+    expect(position).toEqual({ x: 1, y: 1, d: 1 });
   });
 });
--- lib/interpreter/index.ts
+++ lib/interpreter/index.ts
@@ -1,4 +1,5 @@
 import { Grid } from "../grid";
+import { getDirection } from "../position";
 
 export class Interpreter {
   public static parseGrid(gridString: string, spaceDelimiter: string = " ") {
@@ -6,5 +7,15 @@ export class Interpreter {
       .split(spaceDelimiter)
       .map((c) => parseInt(c, 10));
     return new Grid(xMax, yMax);
+  }
+
+  public static parsePosition(
+    positionString: string,
+    spaceDelimiter: string = " "
+  ) {
+    const [x, y, d] = positionString.split(spaceDelimiter);
+    const direction = getDirection(d);
+
+    return { x: parseInt(x, 10), y: parseInt(y, 10), d: direction };
   }
 }
